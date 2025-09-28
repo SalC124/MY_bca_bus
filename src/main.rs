e90e15331e666e4d6cf0298050360353c3126ebc
SalC124 <Saltcal124@gmail.com>
2025-09-28 00:39:35 -0400

    OMG UREQ WORKS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use dioxus::prelude::*;
 
-fn main() {
-    dioxus::launch(App);
-}
-
 #[component]
 fn App() -> Element {
-    rsx! {}
+    let mut response = use_signal(|| "Loading...".to_string());
+
+    let onsubmit = move |_: FormEvent| async move {
+        let res = ureq::get("https://api.ipify.org/?format=json")
+            .call()
+            .unwrap()
+            .body_mut()
+            .read_to_string();
+
+        match res {
+            Ok(text) => response.set(text),
+            Err(err) => response.set(format!("Error: {err}")),
+        }
+    };
+    rsx! {
+        div {
+            id: "test_ureq",
+            form {
+                onsubmit,
+                button { "{response}" },
+            }
+        }
+
+    }
+}
+
+fn main() {
+    dioxus::launch(App);
 }
